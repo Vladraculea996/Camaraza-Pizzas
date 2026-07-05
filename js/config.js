f6ff1256240bb6a428687ead5e0ba690b7e87ecb
Vladraculea996 <158999332+Vladraculea996@users.noreply.github.com>
2026-07-05 17:15:05 -0400

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,21 @@
+const APP_CONFIG = {
+
+  APP_NAME: "Camaraza Pizzas",
+
+  VERSION: "1.2.0",
+
+  DOMAIN: "https://vladraculea996.github.io/Camaraza-Pizzas/",
+
+  COUNTRY: "Cuba",
+
+  LANGUAGES: ["es", "en"],
+
+  DEFAULT_LANGUAGE: "es",
+
+  CURRENCIES: ["CUP", "USD"],
+
+  DEFAULT_CURRENCY: "CUP",
+
+  TIMEZONE: "America/Havana"
+
+};
